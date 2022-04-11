92c43ca75543254bfff1a8ad6bc6f90ef0cc0dd5
Utkatsh Kaule <utkarshkaule7@gmail.com>
2022-04-11 12:49:08 +0530

    commite

---
+++ unit5_c1/src/App.js
@@ -0,0 +1,49 @@
+
+import './App.css';
+import React from 'react';
+function App() {
+  const [runes, runCounter] = React.useState(0);
+  const addRuns = (value)=>{
+    runCounter(runes + value);
+  }
+
+  const [wicket, wCounter] = React.useState(0);
+  const addWickets = (w) => {
+    wCounter(wicket + w );
+    // console.log()
+  }
+
+  // const [counter,oCounter] = React.useState(0);
+ 
+
+  // const [ball, bCounter] = React.useState(0);
+  // const incrementBall = (b,c) =>{
+  //   bCounter(ball + b);
+  //   if(ball === 6){
+  //     ball = 0;
+      
+      
+  //   }
+  // }
+
+
+  return (
+    <div className="App">
+      <h1>INDIA</h1>
+      <h4>Score {runes}</h4>
+      <button className='addRun' onClick={()=>addRuns(1)}>add 1</button>
+      <button className='addRun' onClick={()=>addRuns(4)}>add 4</button>
+      <button className='addRun' onClick={()=>addRuns(6)}>add 6</button>
+
+      <h4>Wicket {wicket}</h4>
+      <button className='addRun' onClick={()=>addWickets(1)}>add 1</button>
+
+
+      {/* <h4>Overs {}.{}</h4>
+      <button className='addRun' onClick={()=>incrementBall(1)}>add 1</button> */}
+
+    </div>
+  );
+}
+
+export default App;
